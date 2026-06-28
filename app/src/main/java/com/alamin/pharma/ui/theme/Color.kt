c9d6fd9fc876b69a028f852d981824fcb8af4d18
AAlgammal23 <143761202+AAlgammal23@users.noreply.github.com>
2026-06-29 02:31:36 +0300

    Create Color.kt

---
+++ app/src/main/java/com/alamin/pharma/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.alamin.pharma.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TealLight = Color(0xCC1ABC9C)
+val TealDark = Color(0xFF16A085)
+val TealExtraLight = Color(0xCCE8F8F5)
+val White = Color(0xFFFFFFFF)
+val Background = Color(0xFFF5F8FA)
+val TextPrimary = Color(0xFF2C3E50)
+val TextSecondary = Color(0xFF7F8C8D)
+val Error = Color(0xFFE74C3C)
+val Success = Color(0xFF27AE60)
+val Warning = Color(0xFFF39C12)

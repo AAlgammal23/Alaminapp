13e7ac55e95a8aad24ded7a03ceff4fa4c91e07a
AAlgammal23 <143761202+AAlgammal23@users.noreply.github.com>
2026-06-29 15:57:06 +0300

    Update ContactUtils.kt

--- app/src/main/java/com/alamin/pharma/utils/ContactUtils.kt
+++ app/src/main/java/com/alamin/pharma/utils/ContactUtils.kt
@@ -1,45 +1,34 @@
 package com.alamin.pharma.utils
 
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 
 object ContactUtils {
-    @Composable
-    fun openWhatsApp(message: String) {
-        val context = LocalContext.current
+    fun openWhatsApp(context: Context, message: String) {
         val phone = "+967774973636"
         val url = "https://api.whatsapp.com/send?phone=$phone&text=${Uri.encode(message)}"
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         context.startActivity(intent)
     }
 
-    @Composable
-    fun call(phone: String) {
-        val context = LocalContext.current
+    fun call(context: Context, phone: String) {
         val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phone"))
         context.startActivity(intent)
     }
 
-    @Composable
-    fun sendEmail(email: String) {
-        val context = LocalContext.current
+    fun sendEmail(context: Context, email: String) {
         val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:$email"))
         context.startActivity(intent)
     }
 
-    @Composable
-    fun openLocation(address: String) {
-        val context = LocalContext.current
+    fun openLocation(context: Context, address: String) {
         val uri = Uri.parse("geo:0,0?q=$address")
         val intent = Intent(Intent.ACTION_VIEW, uri)
         context.startActivity(intent)
     }
 
-    @Composable
-    fun openFacebook(url: String) {
-        val context = LocalContext.current
+    fun openFacebook(context: Context, url: String) {
         val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
         context.startActivity(intent)
     }
